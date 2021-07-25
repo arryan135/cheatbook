f2eca1786488811fe89db610b606ac04e90b084b
arryan <arryan@umich.edu>
2021-07-25 01:03:09 -0400

    fix typescript error and successfully implement caching

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -52,14 +52,14 @@ export const unpkgPathPlugin = () => {
         } 
 
         // Check to see if we have already loaded this file and if it is in the cache
-        const cachedResult = await fileCache.getItem(args.path);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
         // if it is, return it immediately
         if (cachedResult){
           return cachedResult;
         }
         const { data, request } = await axios.get(args.path);
         
-        const result =  {
+        const result: esbuild.OnLoadResult =  {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname
