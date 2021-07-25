9a02f77f30aae72abd2cb9fb29205c5625c1c579
arryan <arryan@umich.edu>
2021-07-25 15:41:50 -0400

    refactor to include a fetch-plugin that loads files/packages

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,46 @@
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+  name: "filecache"
+});
+
+// onLoad file is used to load up a file from the file system
+// if we define an onLoad function in our plugin, we override ESbuild's natural process of trying to access the filesystem
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: "fetch-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // if esbuild is attempting to load the index.js file dont let it do its normal thing
+        // dont let it load a file from the file system. We loaded it for you using the return object
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            // in the contents it tries to find any require/imports and after finding any it runs the 
+            contents: inputCode,
+          };
+        } 
+
+        // Check to see if we have already loaded this file and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        // if it is, return it immediately
+        if (cachedResult){
+          return cachedResult;
+        }
+        const { data, request } = await axios.get(args.path);
+        
+        const result: esbuild.OnLoadResult =  {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname
+        }
+
+        // store in cache if not already in cache
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+    }
+  }
+}
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,12 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from "axios";
-import localforage from "localforage";
-
-const fileCache = localforage.createInstance({
-  name: "filecache"
-});
  
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -31,40 +25,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`                         
         }
-      });
-
-      // onLoad file is used to load up a file from the file system
-      // if we define an onLoad function in our plugin, we override ESbuild's natural process of trying to access the filesystem
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
- 
-        // if esbuild is attempting to load the index.js file dont let it do its normal thing
-        // dont let it load a file from the file system. We loaded it for you using the return object
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            // in the contents it tries to find any require/imports and after finding any it runs the 
-            contents: inputCode,
-          };
-        } 
-
-        // Check to see if we have already loaded this file and if it is in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-        // if it is, return it immediately
-        if (cachedResult){
-          return cachedResult;
-        }
-        const { data, request } = await axios.get(args.path);
-        
-        const result: esbuild.OnLoadResult =  {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname
-        }
-
-        // store in cache
-        await fileCache.setItem(args.path, result);
-        return result;
       });
     },
   };
