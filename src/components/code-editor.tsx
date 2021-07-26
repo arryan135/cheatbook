6e520dce55b672ffe25f5c3d1dd9651e2d06d4a4
arryan <arryan@umich.edu>
2021-07-26 14:47:14 -0400

    add hover format button inside editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,6 +2,7 @@ import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
 import { useRef } from "react";
+import "./code-editor.css"
 
 interface CodeEditorProps{
   initialValue: string;
@@ -37,7 +38,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
   }
 
   return (
-  <div>
+  <div className = "editor-wrapper">
     <button className = "button button-format is-primary is-small" onClick = {onFormatClick}>Format</button>
     <MonacoEditor 
       editorDidMount = {onEditorDidMount}
