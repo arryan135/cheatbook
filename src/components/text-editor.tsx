fec894ae8e63f4d98a16e1b1d2ec65d8bd01eb91
arryan <arryan@umich.edu>
2021-07-27 06:27:30 -0400

    toggle between editor and markdown editor using click listeners

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react";
 import MDEditor from "@uiw/react-md-editor";
+import { fromNodes } from "jscodeshift/src/Collection";
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      setEditing(false);
+    }
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    }
+  }, []);
+
+  if (editing){
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div onClick = {() => setEditing(true)}>
       <MDEditor.Markdown source = {"# Header"} />
     </div>
   )
