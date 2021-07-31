978a23317d81e6f6650b7471eab40606b39909ec
arryan <arryan@umich.edu>
2021-07-31 12:23:51 -0400

    prevent preview window from reloading and blinking every 750ms

--- src/hooks/use-actions.ts
+++ src/hooks/use-actions.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../state";
@@ -5,5 +6,12 @@ import { actionCreators } from "../state";
 export const useActions = () => {
   const dispatch = useDispatch();
 
-  return bindActionCreators(actionCreators, dispatch);
+  // this ensure that action creators are bound one single time
+  // useMemo is almost like useEffect and useState
+  // when something changes inside the diapatch array the first callback function is run
+  // the return value from the callback function is useMemo's return value
+  // All this makes `createbundle` in code-cell.tsx a stable function.
+  return useMemo(() => {
+    return bindActionCreators(actionCreators, dispatch);
+  }, [dispatch]);
 }
