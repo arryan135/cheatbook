d62ff84384a115d4334d5598eaf1564524c0e366
arryan <arryan@umich.edu>
2021-08-01 21:34:22 -0400

    add meaningful messages while success and error handling

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -10,7 +10,16 @@ export const serveCommand = new Command()
     try {
       const dir = path.join(process.cwd(), path.dirname(filename))
       await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      )
     } catch (err){
-      console.log("Heres the problem", err.message);
+      if (err.code === "EADDRINUSE") {
+        console.error("Port is in use. Try running on a different port.");
+      } else{
+        console.log("Heres the problem:", err.message);
+      }
+      // exiting with unsuccessful run of our program
+      process.exit(1);
     }
   });
