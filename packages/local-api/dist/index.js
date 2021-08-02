c322c70eb4ea9d2d406edb950ebafb0020e9a743
arryan <arryan@umich.edu>
2021-08-01 23:46:02 -0400

    setup methods to serve react files while devlopment or production

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,17 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
 var path_1 = __importDefault(require("path"));
-var serve = function (port, filename, dir) {
+var serve = function (port, filename, dir, useProxy) {
     var app = express_1.default();
-    // Apply node's HARTH resolution algorithm to find file location of index.html (absolute path)
-    var packagePath = require.resolve("local-client/build/index.html");
-    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
-    // app.use(createProxyMiddleware({
-    //   target: "http://localhost:3000",
-    //   ws: true,
-    //   logLevel: "silent"
-    // }))
+    if (useProxy) {
+        app.use(http_proxy_middleware_1.createProxyMiddleware({
+            target: "http://localhost:3000",
+            ws: true,
+            logLevel: "silent"
+        }));
+    }
+    else {
+        // Apply node's HARTH resolution algorithm to find file location of index.html (absolute path)
+        var packagePath = require.resolve("local-client/build/index.html");
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on("error", reject);
     });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -2,18 +2,20 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import path from "path";
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (port: number, filename: string, dir: string, useProxy: boolean) => {
   const app = express();
-
-  // Apply node's HARTH resolution algorithm to find file location of index.html (absolute path)
-  const packagePath = require.resolve("local-client/build/index.html");
-  app.use(express.static(path.dirname(packagePath)));
-
-  // app.use(createProxyMiddleware({
-  //   target: "http://localhost:3000",
-  //   ws: true,
-  //   logLevel: "silent"
-  // }))
+  
+  if(useProxy){
+    app.use(createProxyMiddleware({
+      target: "http://localhost:3000",
+      ws: true,
+      logLevel: "silent"
+    }));
+  } else{
+    // Apply node's HARTH resolution algorithm to find file location of index.html (absolute path)
+    const packagePath = require.resolve("local-client/build/index.html");
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
